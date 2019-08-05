69682886f1c31b5e39ed23dc00dcf92b314c046a
runnerro4d <aaditya.n.sinha@gmail.com>
2019-08-05 16:34:12 +1000

    Updates

---
+++ precache-manifest.67087f041c9ab7808b8733f6544216e7.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf07efc28b97cc20b27ee74ba067d798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2c5cdc624bf12089b07",
+    "url": "/static/css/2.ab5dcbdc.chunk.css"
+  },
+  {
+    "revision": "a06b3c7087f5fc4d4079",
+    "url": "/static/css/main.c4a526a5.chunk.css"
+  },
+  {
+    "revision": "e2c5cdc624bf12089b07",
+    "url": "/static/js/2.23a05d38.chunk.js"
+  },
+  {
+    "revision": "a06b3c7087f5fc4d4079",
+    "url": "/static/js/main.19885941.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "979d80388a4c93db3b5db719bdb96cdd",
+    "url": "/static/media/8bit.979d8038.jpeg"
+  },
+  {
+    "revision": "df7d3357a02852a8ffd3316ae5183f78",
+    "url": "/static/media/8thbit.df7d3357.jpeg"
+  },
+  {
+    "revision": "a04b98d98730867ddbbed467b57a5165",
+    "url": "/static/media/Aaditya.a04b98d9.jpg"
+  },
+  {
+    "revision": "e7ecb9902b5d991cc4259b4471bd67b3",
+    "url": "/static/media/Final.e7ecb990.mp4"
+  },
+  {
+    "revision": "8389960ccd51b7ad15b84783a831761f",
+    "url": "/static/media/Itisha.8389960c.jpg"
+  },
+  {
+    "revision": "8569e6a9aa48ca8928b60efffb5280b8",
+    "url": "/static/media/Kartik.8569e6a9.jpg"
+  },
+  {
+    "revision": "a9be260a5e4b561d1beaaae4516056a4",
+    "url": "/static/media/Laveeshka.a9be260a.jpg"
+  },
+  {
+    "revision": "b7e26a480f781cbc60b1be9d744c8a70",
+    "url": "/static/media/Manali.b7e26a48.jpg"
+  },
+  {
+    "revision": "be3b68e146149868d87ac1e5124b49da",
+    "url": "/static/media/Neha.be3b68e1.jpg"
+  },
+  {
+    "revision": "e7d05010ca03548edd0081ce2f12208e",
+    "url": "/static/media/Poster1.e7d05010.png"
+  },
+  {
+    "revision": "a540b9d924e3727bee4f6e17e8d3883f",
+    "url": "/static/media/Poster2.a540b9d9.png"
+  },
+  {
+    "revision": "65e508466cec8192c05874f79a0ebfee",
+    "url": "/static/media/Poster3.65e50846.jpeg"
+  },
+  {
+    "revision": "c7e97f1a28e4353311d6e43afbb64ced",
+    "url": "/static/media/Poster4.c7e97f1a.jpeg"
+  },
+  {
+    "revision": "2b20c192c3e5771d937232ddd4631768",
+    "url": "/static/media/Poster5.2b20c192.jpeg"
+  },
+  {
+    "revision": "90c0707defbf0006ed268d6a44d6ab80",
+    "url": "/static/media/Praggy.90c0707d.jpg"
+  },
+  {
+    "revision": "386cdaea3dbf9099f82f4e02a20fbd7e",
+    "url": "/static/media/Ranginberry.386cdaea.jpg"
+  },
+  {
+    "revision": "b3052444bde43e7f3b22329949077e0c",
+    "url": "/static/media/Ranveer.b3052444.jpg"
+  },
+  {
+    "revision": "1b06d080f1f41fcc06110b9b796c14ad",
+    "url": "/static/media/Thomas.1b06d080.jpg"
+  },
+  {
+    "revision": "ed860b9179a5300cdd023014d8dbe3bd",
+    "url": "/static/media/eightbitlogo.ed860b91.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1d18dde9468444d66fb5b5cae35a8129",
+    "url": "/static/media/wallpaper1.1d18dde9.jpg"
+  }
+]);
